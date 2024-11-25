b26d3d5e2ebdd4800eb915e5b2ef3dd2bc9a5abb
ducphuongidol <136479330+ducphuongidol@users.noreply.github.com>
2024-11-25 23:04:20 +0700

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -34,12 +34,4 @@ gallery.querySelectorAll('.gallery-item').forEach(function (item) {
     item.addEventListener('click', function () {
         item.classList.toggle('full');
     });
-<<<<<<< HEAD
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> ab7d03abd925ccf59c2614f49e368486425c5b66
-=======
-});
->>>>>>> backup-branch
